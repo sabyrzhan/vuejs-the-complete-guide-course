e3b97bff6bd50bab9735003724cb4b0f3118422b
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-04-14 19:30:10 +0300

    Add components related practice exercises

---
+++ 007-vue-cli/src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <h2>{{ friend.name }} {{ friend.isFavorite ? '(Fav)' : ''}}</h2>
+    <button @click="toggleFavorite">Toggle fav</button>
+    <button @click="toggleDetails">Show details</button>
+    <button @click="$emit('delete-friend', friend.id)">Delete Friend</button>
+    <ul v-if="visible">
+      <li>
+        <strong>Phone:</strong>
+        {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['friend'],
+  emits: ['toggle-favorite', 'delete-friend'],
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.visible = !this.visible;
+    },
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.friend.id)
+    }
+  }
+}
+</script>
---
+++ 007-vue-cli/src/components/NewFriend.vue
@@ -0,0 +1,45 @@
+<template>
+<form @submit.prevent>
+  <p>
+    Name: <input v-model="name" />
+  </p>
+  <p>
+    Phone: <input v-model="phone" />
+  </p>
+  <p>
+    Email: <input v-model="email" />
+  </p>
+  <p>
+    <button @click="addContact">Add</button>
+  </p>
+</form>
+</template>
+
+<script>
+export default {
+  emits: ['add-friend'],
+  data() {
+    return {
+      name: '',
+      phone: '',
+      email: ''
+    }
+  },
+  methods: {
+    addContact() {
+      const friend = {
+        id: this.name,
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        isFavorite: false
+      }
+      this.name = '';
+      this.phone = '';
+      this.email = '';
+      this.$emit('add-friend', friend)
+    }
+  }
+}
+</script>
+
