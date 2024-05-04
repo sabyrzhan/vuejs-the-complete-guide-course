eb5223aa38c2b9a7d24a55211c5f31523c27bf4c
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-04 14:26:04 +0300

    009-diving-deeper: add ErrorAlert.vue to show dialog window and use in ManageGoals.vue

---
+++ 009-diving-deeper-into-components/src/components/ErrorAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <dialog open>
+    <h3>Error</h3>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+  dialog {
+    position: fixed;
+    margin: 0;
+    top: 10vh;
+    width: 300px;
+    left: 40%;
+  }
+</style>
--- 009-diving-deeper-into-components/src/components/ManageGoals.vue
+++ 009-diving-deeper-into-components/src/components/ManageGoals.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <h3>Manage Goals</h3>
-    <input type="text" />
+    <input type="text" ref="textInput" />
+    <button @click="handleSaveClick">Save</button>
+    <error-alert v-if="isInvalid">
+      <h3>Please enter the value</h3>
+      <button @click="handleCloseDialog">OK</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+import ErrorAlert from "@/components/ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert
+  },
+  data() {
+    return {
+      isInvalid: false
+    }
+  },
+  methods: {
+    handleSaveClick() {
+      const value = this.$refs.textInput.value
+      if (value.trim() === '') {
+        this.isInvalid = true
+      }
+    },
+    handleCloseDialog() {
+      this.isInvalid = false
+    }
+  }
+}
+</script>
