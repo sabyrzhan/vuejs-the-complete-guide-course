d6b95a3ed3bece3ee00a06aebb7dd1ce29dd09e8
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-11 21:23:52 +0300

    014-animations: add animation with transition and transform of the box

--- 014-animations/src/App.vue
+++ 014-animations/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{animate: doAnimate}"></div>
+    <button @click="animate">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,7 +15,7 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { dialogIsVisible: false, doAnimate: false };
   },
   methods: {
     showDialog() {
@@ -24,6 +24,9 @@ export default {
     hideDialog() {
       this.dialogIsVisible = false;
     },
+    animate() {
+      this.doAnimate = true;
+    }
   },
 };
 </script>
@@ -57,6 +60,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: transform 1s ease-in-out;
 }
 .container {
   max-width: 40rem;
@@ -68,5 +72,9 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+
+.animate {
+  transform: translateX(-150px);
 }
 </style>
