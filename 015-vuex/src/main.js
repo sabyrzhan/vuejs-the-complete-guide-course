8c613d9226ff89c71815aaaafeb91799ceab0b44
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-12 18:32:29 +0300

    015-vuex: calling getter in component and in vuex getters

--- 015-vuex/src/main.js
+++ 015-vuex/src/main.js
@@ -13,6 +13,18 @@ const store = createStore({
     increment(state, value1) {
       state.counter += value1;
     }
+  },
+  getters: {
+    counter(state, getters) {
+      if (state.counter === 0) {
+        return getters.defaultIncrementer
+      } else {
+        return state.counter
+      }
+    },
+    defaultIncrementer() {
+      return 10
+    }
   }
 })
 
