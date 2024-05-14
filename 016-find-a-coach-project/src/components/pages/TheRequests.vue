dba9753666b5b28c3cc4033660d18d16ab8d9013
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-14 20:30:23 +0300

    016-find-a-coach-project: implement TheRequests.vue page

--- 016-find-a-coach-project/src/components/pages/TheRequests.vue
+++ 016-find-a-coach-project/src/components/pages/TheRequests.vue
@@ -1,7 +1,8 @@
 <template>
   <the-base-card>
     <h2>Requests Received</h2>
-    <request-card v-for="req in requests" :request="req"></request-card>
+    <request-card v-for="req in requests" :request="req" v-if="requests.length !== 0"></request-card>
+    <h3 v-else>No requests found.</h3>
   </the-base-card>
 </template>
 <script>
@@ -18,7 +19,7 @@ export default {
 </script>
 
 <style scoped>
-h2 {
+h2, h3 {
   text-align: center;
 }
 </style>
--- 016-find-a-coach-project/src/store/requests.js
+++ 016-find-a-coach-project/src/store/requests.js
@@ -2,18 +2,7 @@ export default {
     namespaced: true,
     state() {
         return {
-            requests: [
-                {
-                    id: 1,
-                    email: 'some@email.com',
-                    message: 'Some message 1',
-                },
-                {
-                    id: 2,
-                    email: 'some2@email.com',
-                    message: 'Some message 2',
-                }
-            ]
+            requests: []
         }
     },
     mutations: {
