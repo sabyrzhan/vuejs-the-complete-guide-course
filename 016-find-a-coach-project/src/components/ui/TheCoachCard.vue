a798c9d39e7325df3c3422bba58afdce78be2611
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-16 21:19:52 +0300

    016-find-a-coach-project: use dynamic path instead of static in coach links

--- 016-find-a-coach-project/src/components/ui/TheCoachCard.vue
+++ 016-find-a-coach-project/src/components/ui/TheCoachCard.vue
@@ -4,8 +4,8 @@
     <p><strong>{{ coach.price }}</strong></p>
     <the-badge v-for="tag in coach.tags" :key="tag" :css-class="tag">{{ tag }}</the-badge>
     <div class="buttons">
-      <the-button :link="'/coaches/' + coach.id + '/contact'">Contact</the-button>
-      <the-button css-class="primary" :link="'/coaches/' + coach.id + '/details'">View Details</the-button>
+      <the-button :link="coachContactLink">Contact</the-button>
+      <the-button css-class="primary" :link="coachDetailsLink">View Details</the-button>
     </div>
   </div>
 </template>
@@ -15,7 +15,15 @@ import TheButton from "@/components/ui/TheButton.vue";
 
 export default {
   components: {TheButton, TheBadge},
-  props: ['coach']
+  props: ['coach'],
+  computed: {
+    coachContactLink() {
+      return this.$route.path + '/' + this.coach.id + '/contact'
+    },
+    coachDetailsLink() {
+      return this.$route.path + '/' + this.coach.id + '/details'
+    }
+  }
 }
 </script>
 
