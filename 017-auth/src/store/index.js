63bafd379fac22882e817b010b2625f82a837c81
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-17 20:52:11 +0300

    017-auth: add auth store module

--- 017-auth/src/store/index.js
+++ 017-auth/src/store/index.js
@@ -2,21 +2,13 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
+import authModule from '@/store/modules/auth';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
-    requests: requestsModule
-  },
-  state() {
-    return {
-      userId: 'c3'
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    }
+    requests: requestsModule,
+    auth: authModule
   }
 });
 
