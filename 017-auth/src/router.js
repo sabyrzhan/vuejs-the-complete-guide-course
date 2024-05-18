99ad4e64810691a87736758891dfb975d47959e8
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-18 17:37:48 +0300

    017-auth: use deferred functions instead of defineAsyncComponent in router

--- 017-auth/src/router.js
+++ 017-auth/src/router.js
@@ -8,13 +8,12 @@ import CoachesList from './pages/coaches/CoachesList.vue';
 import NotFound from './pages/NotFound.vue';
 // import UserAuth from '@/pages/auth/UserAuth.vue';
 import store from '@/store/index';
-import { defineAsyncComponent } from 'vue';
 
-const CoachDetail = defineAsyncComponent(() => import('@/pages/coaches/CoachDetail.vue'));
-const CoachRegistration = defineAsyncComponent(() => import('@/pages/coaches/CoachRegistration.vue'));
-const ContactCoach = defineAsyncComponent(() => import('@/pages/requests/ContactCoach.vue'));
-const RequestsReceived = defineAsyncComponent(() => import('@/pages/requests//RequestsReceived.vue'));
-const UserAuth = defineAsyncComponent(() => import('@/pages/auth/UserAuth.vue'))
+const CoachDetail = () => import('@/pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('@/pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('@/pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('@/pages/requests//RequestsReceived.vue');
+const UserAuth = () => import('@/pages/auth/UserAuth.vue');
 
 const router = createRouter({
   history: createWebHistory(),
