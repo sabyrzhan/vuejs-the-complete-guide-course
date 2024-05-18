e7698ddf08d5bed35d61ef94d277a1ae9fc5e7ca
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-18 20:45:46 +0300

    019-composition-api: expense tracker p2: finish convert from Options API to Composition API

--- 019-practice2-core-building-blocks/src/App.vue
+++ 019-practice2-core-building-blocks/src/App.vue
@@ -20,31 +20,27 @@
 </template>
 
 <script>
+import {computed, ref, watch} from "vue";
+
 export default {
-  data() {
-    return {
-      availableFunds: 100,
-      currentExpenses: 0,
-      enteredExpense: 0,
-    };
-  },
-  computed: {
-    remainingFunds() {
-      return this.availableFunds - this.currentExpenses;
-    },
-  },
-  methods: {
-    addExpense() {
-      this.currentExpenses += this.enteredExpense;
-    },
-  },
-  watch: {
-    remainingFunds(val) {
+  setup() {
+    const availableFunds = ref(100)
+    const currentExpenses = ref(0)
+    const enteredExpense = ref(0)
+    const remainingFunds = computed(() => {
+      return availableFunds.value - currentExpenses.value
+    })
+    const addExpense = () => {
+      currentExpenses.value += enteredExpense.value
+    }
+    watch(remainingFunds, (val) => {
       if (val < 0) {
         alert('You are broke!');
       }
-    },
-  },
+    })
+
+    return {availableFunds, currentExpenses, enteredExpense, remainingFunds, addExpense}
+  }
 };
 </script>
 
