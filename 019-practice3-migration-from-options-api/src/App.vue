d84412d02f32a084e9eba095cbe864f6572ad206
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-19 16:29:58 +0300

    019-composition-api: migration p2: finish migrate App.vue

--- 019-practice3-migration-from-options-api/src/App.vue
+++ 019-practice3-migration-from-options-api/src/App.vue
@@ -10,23 +10,22 @@ import USER_DATA from './dummy-data.js';
 
 import UserList from './components/users/UserList.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
+import {ref} from "vue";
 
 export default {
   components: {
     UserList,
     ProjectsList,
   },
-  data() {
-    return {
-      selectedUser: null,
-      activeUsers: USER_DATA,
-    };
-  },
-  methods: {
-    selectUser(uid) {
-      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-    },
-  },
+  setup() {
+    const selectedUser = ref(null)
+    const activeUsers = USER_DATA
+    const selectUser = (uid) => {
+      selectedUser.value = this.activeUsers.find((usr) => usr.id === uid);
+    }
+
+    return {selectedUser, activeUsers, selectUser}
+  }
 };
 </script>
 
