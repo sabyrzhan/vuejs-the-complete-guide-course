4f2722d96c661d23c66991dc5670cab179a40235
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-05-19 17:21:37 +0300

    019-composition-api: migration p2: finish BaseSearch.vue

--- 019-practice3-migration-from-options-api/src/components/UI/BaseSearch.vue
+++ 019-practice3-migration-from-options-api/src/components/UI/BaseSearch.vue
@@ -8,11 +8,12 @@
 export default {
   props: ['searchTerm'],
   emits: ['search'],
-  methods: {
-    search(event) {
-      this.$emit('search', event.target.value);
-    },
-  },
+  setup(props, ctx) {
+    const search = (event) => {
+      ctx.emit('search', event.target.value);
+    }
+    return { search }
+  }
 };
 </script>
 
